d8e559c8269ad6003405bf7948089f8af6963ad1
Daniel Machado <needforock@gmail.com>
2023-08-29 09:59:21 -0400

    bug

--- backend/src/middlewares/is_8_char.js
+++ backend/src/middlewares/is_8_char.js
@@ -5,7 +5,7 @@ export default function is_8_char(req, res, next) {
         } else {
             return res.status(400).json({
                 success: true,
-                message: "password must have 8 characters at least"
+                message: "password must have 8  characters at least"
             })
         }
     } catch (error) {
