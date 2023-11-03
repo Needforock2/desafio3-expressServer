a9a382934414fe26208698e41c6a923d3c11e472
Daniel Machado <needforock@gmail.com>
2023-11-03 13:02:37 -0300

    refactor

--- backend/src/dao/mongo/products.mongo.js
+++ backend/src/dao/mongo/products.mongo.js
@@ -74,12 +74,9 @@ export default class ProductPersistance {
         }
       } else {
         let one = await Product.findById(id);
-        console.log("owner", owner.mail)
-        console.log("one", one.owner);
-        console.log(id)
         if (one) {
           if (one.owner === owner.mail) {
-            let deleted = await Product.findOneAndDelete({_id: id});
+           await Product.findOneAndDelete({_id: id});
             return {
               success: true,
               message: `product id: ${one._id} deleted`,
