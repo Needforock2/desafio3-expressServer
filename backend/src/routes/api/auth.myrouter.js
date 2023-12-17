b5c5a21900374f2c05357879d213e5d982693eb5
Daniel Machado <needforock@gmail.com>
2023-12-17 13:56:41 -0300

    session

--- backend/src/routes/api/auth.myrouter.js
+++ backend/src/routes/api/auth.myrouter.js
@@ -57,7 +57,7 @@ export default class AuthRouter extends MyRouter {
           if (response) {
       
             return res
-              .cookie("token", req.token, {
+              .cookie("token", req.session.token, {
                // domain: [`https://prismatic-conkies-0cf372.netlify.app`,"localhost:8080/"],
                 maxAge: 60 * 60 * 25 * 7 * 1000,
                 //httpOnly: true,
